bac1a81d14334b49dfada6565c50d389d2e3994c
eugenechen-ourtimes <eugene.chen19971108@gmail.com>
2023-12-04 00:07:13 +0800

    modify css 7

--- src/sportsman/Notification.tsx
+++ src/sportsman/Notification.tsx
@@ -16,9 +16,7 @@ const Notification = (props: NotificationProps) => {
             <div className="notification-space1"/>
             <div className="notification-item">
                 <div>
-                    [即將開始] 台大體育館 - 羽球 1AF2
-                </div>
-                <div>
+                    [即將開始] 台大體育館 - 羽球 1AF2<br/>
                     2023-10-14 11:00-15:00
                 </div>
             </div>
