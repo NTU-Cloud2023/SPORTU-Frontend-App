2ce5fa5d69ca2b669cf2cc5fa08d278c48048962
ren-joey <tsuyiren@gmail.com>
2023-12-15 18:59:27 +0800

    style: 💄 lint update

--- src/utils/getDistanceFromLatLonInKm.ts
+++ src/utils/getDistanceFromLatLonInKm.ts
@@ -9,8 +9,7 @@ const getDistanceFromLatLonInKm = (lat1: number, lon1: number, lat2: number, lon
     const a =
       Math.sin(dLat/2) * Math.sin(dLat/2) +
       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon/2) * Math.sin(dLon/2)
-      ;
+      Math.sin(dLon/2) * Math.sin(dLon/2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     const d = R * c; // Distance in km
     return d;
