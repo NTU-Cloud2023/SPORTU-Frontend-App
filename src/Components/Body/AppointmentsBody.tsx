ce5a4cfedfaeb8034ca443131f2cebcc758a3335
ren-joey <tsuyiren@gmail.com>
2023-12-21 03:26:43 +0800

    tidy: 🧹 remove unused codes

--- src/Components/Body/AppointmentsBody.tsx
+++ src/Components/Body/AppointmentsBody.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import AppointmentListItem from '../AppointmentListItem';
 import Gap from '../Gap';
 import LeftSubTitle from '../Title/LeftSubTitle';
